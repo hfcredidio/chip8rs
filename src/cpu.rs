c63136ba8d45a6c4bc689b0bb3a95cbb36632422
heitor <heitor@redbaron.local>
2018-10-23 21:41:10 +0100

    Execute opcodes and print them

--- src/cpu.rs
+++ src/cpu.rs
@@ -16,8 +16,8 @@ impl Cpu {
             v: [0; 0x10],
             dt: 0,
             i: 0,
-            pc: 0,
-            sp: 0,
+            pc: 0x200,
+            sp: 0xFFF,
             st: 0,
         }
     }
@@ -45,6 +45,21 @@ impl Cpu {
         let n2 = self.read_memory(self.sp + 1);
         self.sp += 2;
         (n1 as u16) << 8 | n2 as u16
+    }
+    pub fn load_rom(&mut self, buf: &[u8], size: usize) {
+        for i in 0..size {
+            self.ram[i + 0x200] = buf[i];
+        }
+    }
+    pub fn read_word(&mut self) {
+        let opcode = (self.read_memory(self.pc) as u16) << 8 | self.read_memory(self.pc + 1) as u16;
+        println!("{:X}\t{}", self.pc, format_opcode(opcode));
+        self.call_opcode(opcode);
+        self.pc += 2
+    }
+    pub fn decrement_timers(&mut self) {
+        self.dt -= if self.dt > 0 { 1 } else { 0 };
+        self.st -= if self.st > 0 { 1 } else { 0 };
     }
 
     pub fn call_opcode(&mut self, word: u16) {
@@ -97,12 +112,12 @@ impl Cpu {
         self.pc = self.pop_stack();
     }
     fn JP(&mut self, addr: u16) {
-        self.pc = addr;
+        self.pc = addr - 2;
     }
     fn CALL(&mut self, addr: u16) {
         let pc = self.pc;
         self.push_stack(pc);
-        self.pc = addr;
+        self.pc = addr - 2;
     }
     fn SE(&mut self, x: u8, byte: u8) {
         let xval = self.read_register(x);
@@ -153,9 +168,9 @@ impl Cpu {
         let xval = self.read_register(x);
         let yval = self.read_register(y);
         let res: u16 = (xval as u16) + (yval as u16);
-        let carry = (res & 0xFF00) >> 7;
+        let carry = (res & 0xFF00) >> 8;
         self.write_register(x, res as u8);
-        self.write_register(0xF, carry as u8);
+        self.write_register(0xF, (carry != 0) as u8);
     }
     fn SUB(&mut self, x: u8, y: u8) {
         let xval = self.read_register(x);
@@ -194,7 +209,7 @@ impl Cpu {
         self.i = addr;
     }
     fn JPV0(&mut self, addr: u16) {
-        self.pc = self.read_register(0x0) as u16 + addr;
+        self.pc = self.read_register(0x0) as u16 + addr - 2;
     }
     fn RND(&mut self, x: u8, byte: u8) {
         let rnd = 0;
@@ -259,9 +274,56 @@ impl Cpu {
 
 fn word_to_nibbles(word: u16) -> (u8, u8, u8, u8) {
     (
-        ((word & 0xF000) >> 32) as u8,
-        ((word & 0x0F00) >> 16) as u8,
-        ((word & 0x00F0) >>  8) as u8,
+        ((word & 0xF000) >> 12) as u8,
+        ((word & 0x0F00) >>  8) as u8,
+        ((word & 0x00F0) >>  4) as u8,
         ((word & 0x000F) >>  0) as u8,
     )
+}
+
+
+
+pub fn format_opcode(word: u16) -> String {
+    let (c, Vx, Vy, nibble) = word_to_nibbles(word);
+    let addr = word & 0x0FFF;
+    let byte = (word & 0x00FF)  as u8;
+    let fmt = match (c, Vx, Vy, nibble) {
+        (0x0, 0x0, 0xE, 0x0) => "CLS".to_string(),
+        (0x0, 0x0, 0xE, 0xE) => "RET".to_string(),
+        (0x0,   _,   _,   _) => format!("SYS\t{:X}", addr),
+        (0x1,   _,   _,   _) => format!("JP\t{:X}", addr),
+        (0x2,   _,   _,   _) => format!("CALL\t{:X}", addr),
+        (0x3,   _,   _,   _) => format!("SE\t[{:X}] {:X}", Vx, byte),
+        (0x4,   _,   _,   _) => format!("SNE\t[{:X}] {:X}", Vx, byte),
+        (0x5,   _,   _, 0x0) => format!("SE2\t[{:X}] [{:X}]", Vx, Vy),
+        (0x6,   _,   _,   _) => format!("LD\t[{:X}] {:X}", Vx, byte),
+        (0x7,   _,   _,   _) => format!("ADD\t[{:X}] {:X}", Vx, byte),
+        (0x8,   _,   _, 0x0) => format!("LD2\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x1) => format!("OR\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x2) => format!("AND\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x3) => format!("XOR\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x4) => format!("ADD2\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x5) => format!("SUB\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x6) => format!("SHR\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0x7) => format!("SUBN\t[{:X}] [{:X}]", Vx, Vy),
+        (0x8,   _,   _, 0xE) => format!("SHL\t[{:X}] [{:X}]", Vx, Vy),
+        (0x9,   _,   _, 0x0) => format!("SNE2\t[{:X}] [{:X}]", Vx, Vy),
+        (0xA,   _,   _,   _) => format!("LDI\t{:X}", addr),
+        (0xB,   _,   _,   _) => format!("JPV0\t{:X}", addr),
+        (0xC,   _,   _,   _) => format!("RND\t[{:X}] {:X}", Vx, byte),
+        (0xD,   _,   _,   _) => format!("DRW\t[{:X}] [{:X}] {:X}", Vx, Vy, nibble),
+        (0xE,   _, 0x9, 0xE) => format!("SKP\t[{:X}]", Vx),
+        (0xE,   _, 0xA, 0x1) => format!("SKNP\t[{:X}]", Vx),
+        (0xF,   _, 0x0, 0x7) => format!("LDT\t[{:X}]", Vx),
+        (0xF,   _, 0x0, 0xA) => format!("LDK\t[{:X}]", Vx),
+        (0xF,   _, 0x1, 0x5) => format!("LDT2\t[{:X}]", Vx),
+        (0xF,   _, 0x1, 0x8) => format!("LDS\t[{:X}]", Vx),
+        (0xF,   _, 0x1, 0xE) => format!("ADDI\t[{:X}]", Vx),
+        (0xF,   _, 0x2, 0x9) => format!("LDD\t[{:X}]", Vx),
+        (0xF,   _, 0x3, 0x3) => format!("LDB\t[{:X}]", Vx),
+        (0xF,   _, 0x5, 0x5) => format!("STR\t[{:X}]", Vx),
+        (0xF,   _, 0x6, 0x5) => format!("LDR\t[{:X}]", Vx),
+        (  _,   _,   _,   _) => format!("SYS\t{:X}", addr),
+    };
+    fmt
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 mod cpu;
 
 use cpu::Cpu;
+use std::io::Read;
 
 fn main() {
-    let cpu = Cpu::new();
+    let mut cpu = Cpu::new();
+    let mut file = std::fs::File::open("games/TANK").unwrap(); 
+    let mut data: [u8; 0x1000] = [0; 0x1000];
+    let size = file.read(&mut data).unwrap();
+    cpu.load_rom(&data, size);
+    while true {
+        cpu.decrement_timers();
+        cpu.read_word();
+        std::thread::sleep(std::time::Duration::from_millis(17));
+    }
+    println!("{}", size);
 }
